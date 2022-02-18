ae2829bea66f5a0c687e2d4946bf27198600a91d
prem sai <56483749+premsai1503@users.noreply.github.com>
2022-02-18 21:02:52 +0530

    Delete login 2 image gradient directory

--- login 2 image gradient/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:login/login.dart';
-
-void main() {
-  runApp(MaterialApp(
-    home: AuthPage(),
-    debugShowCheckedModeBanner: false,
-  ));
-}
--- login 2 image gradient/readme.md
+++
@@ -1,4 +0,0 @@
-
-### Output
-<br>
-<img src="screenshots/img-grad-login.png" width="300" height=auto>
